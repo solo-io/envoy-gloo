19b5a0a4a240735174fc3b35dcc043743eb4ce0e
Tal Nordan <tal.nordan@solo.io>
2018-02-08 15:07:00 -0800

    Fix formatting

--- source/common/http/functional_stream_decoder_base.h
+++ source/common/http/functional_stream_decoder_base.h
@@ -2,63 +2,63 @@
 
 #include "envoy/server/filter_config.h"
 #include "envoy/upstream/cluster_manager.h"
+
 #include "common/protobuf/utility.h"
 
 namespace Envoy {
 namespace Http {
 
-
 using Envoy::Server::Configuration::FactoryContext;
 
 class FunctionalFilterBase : public StreamDecoderFilter,
-                     public Logger::Loggable<Logger::Id::filter> {
+                             public Logger::Loggable<Logger::Id::filter> {
 public:
-  FunctionalFilterBase(FactoryContext& ctx, const std::string& childname) : 
-   decoder_callbacks_(nullptr),
-    cm_(ctx.clusterManager()),random_(ctx.random()), childname_(childname), 
-   cluster_info_(nullptr), spec_(nullptr) {}
+  FunctionalFilterBase(FactoryContext &ctx, const std::string &childname)
+      : decoder_callbacks_(nullptr), cm_(ctx.clusterManager()),
+        random_(ctx.random()), childname_(childname), cluster_info_(nullptr),
+        spec_(nullptr) {}
   virtual ~FunctionalFilterBase();
 
   // Http::StreamFilterBase
 
   // Http::StreamDecoderFilter
-  FilterHeadersStatus decodeHeaders(HeaderMap &headers, bool end_stream) override;
+  FilterHeadersStatus decodeHeaders(HeaderMap &headers,
+                                    bool end_stream) override;
   FilterDataStatus decodeData(Buffer::Instance &data, bool end_stream) override;
 
   FilterTrailersStatus decodeTrailers(HeaderMap &trailers) override;
 
-  void setDecoderFilterCallbacks(StreamDecoderFilterCallbacks &decoder_callbacks) override {
-      decoder_callbacks_ = &decoder_callbacks;
+  void setDecoderFilterCallbacks(
+      StreamDecoderFilterCallbacks &decoder_callbacks) override {
+    decoder_callbacks_ = &decoder_callbacks;
   }
 
 protected:
-  const ProtobufWkt::Struct& getFunctionSpec();
+  const ProtobufWkt::Struct &getFunctionSpec();
   StreamDecoderFilterCallbacks *decoder_callbacks_;
 
   virtual FilterHeadersStatus functionDecodeHeaders(HeaderMap &m, bool e) PURE;
   virtual FilterDataStatus functionDecodeData(Buffer::Instance &, bool) PURE;
   virtual FilterTrailersStatus functionDecodeTrailers(HeaderMap &) PURE;
 
-
 private:
-  Upstream::ClusterManager& cm_;
-  Envoy::Runtime::RandomGenerator& random_;
-  const std::string& childname_;
+  Upstream::ClusterManager &cm_;
+  Envoy::Runtime::RandomGenerator &random_;
+  const std::string &childname_;
 
   Upstream::ClusterInfoConstSharedPtr cluster_info_;
-  const ProtobufWkt::Struct* spec_; 
-
+  const ProtobufWkt::Struct *spec_;
 
   void tryToGetSpec();
-  void findSingleFunction(Upstream::ClusterInfoConstSharedPtr&& info, const ProtobufWkt::Struct& filter_metadata_struct);
-  void tryToGetSpecFromCluster(const std::string& funcname, Upstream::ClusterInfoConstSharedPtr&& clusterinfo);
-  bool isOurCluster(const Upstream::ClusterInfoConstSharedPtr& clusterinfo);
-  bool active() {return spec_ != nullptr;}
+  void findSingleFunction(Upstream::ClusterInfoConstSharedPtr &&info,
+                          const ProtobufWkt::Struct &filter_metadata_struct);
+  void
+  tryToGetSpecFromCluster(const std::string &funcname,
+                          Upstream::ClusterInfoConstSharedPtr &&clusterinfo);
+  bool isOurCluster(const Upstream::ClusterInfoConstSharedPtr &clusterinfo);
+  bool active() { return spec_ != nullptr; }
   void error();
-
 };
 
-
-
-}
-}
+} // namespace Http
+} // namespace Envoy
--- test/common/http/functional_stream_decoder_base_test.cc
+++ test/common/http/functional_stream_decoder_base_test.cc
@@ -1,19 +1,15 @@
 #include <iostream>
 
 #include "common/http/functional_stream_decoder_base.h"
-
 #include "common/protobuf/utility.h"
-
-#include "test/test_common/utility.h"
-
 #include "common/router/config_impl.h"
 
-#include "fmt/format.h"
-
-#include "test/mocks/upstream/mocks.h"
-#include "test/mocks/server/mocks.h"
 #include "test/mocks/common.h"
+#include "test/mocks/server/mocks.h"
+#include "test/mocks/upstream/mocks.h"
+#include "test/test_common/utility.h"
 
+#include "fmt/format.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
@@ -29,72 +25,77 @@ namespace Envoy {
 namespace Http {
 
 class FunctionalFilterTester : public FunctionalFilterBase {
-  public:
-  FunctionalFilterTester(FactoryContext& ctx, const std::string& childname) : FunctionalFilterBase(ctx, childname),
-functionDecodeHeadersCalled_(false),
-functionDecodeDataCalled_(false),
-functionDecodeTrailersCalled_(false){}
+public:
+  FunctionalFilterTester(FactoryContext &ctx, const std::string &childname)
+      : FunctionalFilterBase(ctx, childname),
+        functionDecodeHeadersCalled_(false), functionDecodeDataCalled_(false),
+        functionDecodeTrailersCalled_(false) {}
 
-  const ProtobufWkt::Struct& getSpec() {return getFunctionSpec();}
+  const ProtobufWkt::Struct &getSpec() { return getFunctionSpec(); }
 
-
-   virtual void onDestroy() override{}
-virtual FilterHeadersStatus functionDecodeHeaders(HeaderMap &, bool) override {
-functionDecodeHeadersCalled_ = true;
+  virtual void onDestroy() override {}
+  virtual FilterHeadersStatus functionDecodeHeaders(HeaderMap &,
+                                                    bool) override {
+    functionDecodeHeadersCalled_ = true;
     return FilterHeadersStatus::Continue;
-}
-virtual FilterDataStatus functionDecodeData(Buffer::Instance &, bool) override {
-functionDecodeDataCalled_ = true;
+  }
+  virtual FilterDataStatus functionDecodeData(Buffer::Instance &,
+                                              bool) override {
+    functionDecodeDataCalled_ = true;
     return FilterDataStatus::Continue;
-
-}
-virtual FilterTrailersStatus functionDecodeTrailers(HeaderMap &) override {
-functionDecodeTrailersCalled_ = true;
+  }
+  virtual FilterTrailersStatus functionDecodeTrailers(HeaderMap &) override {
+    functionDecodeTrailersCalled_ = true;
     return FilterTrailersStatus::Continue;
-
-}
-bool functionDecodeHeadersCalled_;
-bool functionDecodeDataCalled_;
-bool functionDecodeTrailersCalled_;
+  }
+  bool functionDecodeHeadersCalled_;
+  bool functionDecodeDataCalled_;
+  bool functionDecodeTrailersCalled_;
 };
-
 
 class FuncitonFilterTest : public testing::Test {
 public:
-  FuncitonFilterTest() :childname_("childfilter"){}
+  FuncitonFilterTest() : childname_("childfilter") {}
 
 protected:
-  void SetUp() override  {
-    
+  void SetUp() override {
+
     initFilter();
 
-    Upstream::MockClusterInfo& info = *factory_context_.cluster_manager_.thread_local_cluster_.cluster_.info_;
-    ON_CALL(info, metadata())
-    .WillByDefault(ReturnRef(cluster_metadata_));
+    Upstream::MockClusterInfo &info =
+        *factory_context_.cluster_manager_.thread_local_cluster_.cluster_.info_;
+    ON_CALL(info, metadata()).WillByDefault(ReturnRef(cluster_metadata_));
 
-    Router::MockRouteEntry& routerentry = filter_callbacks_.route_->route_entry_;
-    ON_CALL(routerentry, metadata())
-      .WillByDefault(ReturnRef(route_metadata_));
-
+    Router::MockRouteEntry &routerentry =
+        filter_callbacks_.route_->route_entry_;
+    ON_CALL(routerentry, metadata()).WillByDefault(ReturnRef(route_metadata_));
   }
 
   void initFilter() {
-    filter_ = std::make_shared<FunctionalFilterTester>(factory_context_, childname_);
+    filter_ =
+        std::make_shared<FunctionalFilterTester>(factory_context_, childname_);
     filter_->setDecoderFilterCallbacks(filter_callbacks_);
   }
 
   void initclustermeta() {
 
-  //TODO use const
-    ProtobufWkt::Struct& functionsstruct = (*cluster_metadata_.mutable_filter_metadata())["io.solo.function_router"];
-    ProtobufWkt::Value& functionstructvalue = (*functionsstruct.mutable_fields())["functions"];
-    ProtobufWkt::Struct* functionstruct = functionstructvalue.mutable_struct_value();
-    ProtobufWkt::Value& functionstructspecvalue = (*functionstruct->mutable_fields())["funcname"];
+    // TODO use const
+    ProtobufWkt::Struct &functionsstruct =
+        (*cluster_metadata_
+              .mutable_filter_metadata())["io.solo.function_router"];
+    ProtobufWkt::Value &functionstructvalue =
+        (*functionsstruct.mutable_fields())["functions"];
+    ProtobufWkt::Struct *functionstruct =
+        functionstructvalue.mutable_struct_value();
+    ProtobufWkt::Value &functionstructspecvalue =
+        (*functionstruct->mutable_fields())["funcname"];
 
     functionsspecstruct_ = functionstructspecvalue.mutable_struct_value();
 
-    // mark the cluster as functional (i.e. the function filter has claims on it.)
-    (*cluster_metadata_.mutable_filter_metadata())[childname_] = ProtobufWkt::Struct();
+    // mark the cluster as functional (i.e. the function filter has claims on
+    // it.)
+    (*cluster_metadata_.mutable_filter_metadata())[childname_] =
+        ProtobufWkt::Struct();
   }
 
   void initroutemeta() {
@@ -103,23 +104,22 @@ protected:
     functionvalue.set_string_value("funcname");
 
     ProtobufWkt::Struct routefunctionmeta;
-    (*routefunctionmeta.mutable_fields())["function"]  = functionvalue;
+    (*routefunctionmeta.mutable_fields())["function"] = functionvalue;
 
-  //TODO use const
-    (*route_metadata_.mutable_filter_metadata())["io.solo.function_router"] = routefunctionmeta;
-    
-
+    // TODO use const
+    (*route_metadata_.mutable_filter_metadata())["io.solo.function_router"] =
+        routefunctionmeta;
   }
 
   NiceMock<Envoy::Http::MockStreamDecoderFilterCallbacks> filter_callbacks_;
   NiceMock<Envoy::Server::Configuration::MockFactoryContext> factory_context_;
-  NiceMock<Envoy::Event::MockTimer>* attachmentTimeout_timer_{};
+  NiceMock<Envoy::Event::MockTimer> *attachmentTimeout_timer_{};
   std::shared_ptr<FunctionalFilterTester> filter_;
-  std::deque<Envoy::Http::AsyncClient::Callbacks*> callbacks_;
+  std::deque<Envoy::Http::AsyncClient::Callbacks *> callbacks_;
 
-  ProtobufWkt::Struct* functionsspecstruct_;
+  ProtobufWkt::Struct *functionsspecstruct_;
 
-  envoy::api::v2::Metadata route_metadata_; 
+  envoy::api::v2::Metadata route_metadata_;
   envoy::api::v2::Metadata cluster_metadata_;
 
   std::string childname_;
@@ -127,9 +127,9 @@ protected:
 
 TEST_F(FuncitonFilterTest, NothingConfigured) {
 
-    Envoy::Http::TestHeaderMapImpl headers{{":method", "GET"},
-                                           {":authority", "www.solo.io"},
-                                           {":path", "/getsomething"}};
+  Envoy::Http::TestHeaderMapImpl headers{{":method", "GET"},
+                                         {":authority", "www.solo.io"},
+                                         {":path", "/getsomething"}};
   filter_->decodeHeaders(headers, true);
 
   EXPECT_FALSE(filter_->functionDecodeHeadersCalled_);
@@ -143,16 +143,15 @@ TEST_F(FuncitonFilterTest, HaveRouteMeta) {
   auto clustername = filter_callbacks_.route_->route_entry_.cluster_name_;
   EXPECT_CALL(factory_context_.cluster_manager_, get(clustername));
 
-    Envoy::Http::TestHeaderMapImpl headers{{":method", "GET"},
-                                           {":authority", "www.solo.io"},
-                                           {":path", "/getsomething"}};
+  Envoy::Http::TestHeaderMapImpl headers{{":method", "GET"},
+                                         {":authority", "www.solo.io"},
+                                         {":path", "/getsomething"}};
   filter_->decodeHeaders(headers, true);
 
-  
-  const ProtobufWkt::Struct& receivedspec = filter_->getSpec();
-  const ProtobufWkt::Struct* receivedspecptr = &receivedspec;
+  const ProtobufWkt::Struct &receivedspec = filter_->getSpec();
+  const ProtobufWkt::Struct *receivedspecptr = &receivedspec;
   // compare pointers - to make sure no copy happened along the way.
-  const ProtobufWkt::Struct* origspecptr = functionsspecstruct_;
+  const ProtobufWkt::Struct *origspecptr = functionsspecstruct_;
   EXPECT_EQ(origspecptr, receivedspecptr);
 
   EXPECT_TRUE(filter_->functionDecodeHeadersCalled_);
@@ -165,14 +164,14 @@ TEST_F(FuncitonFilterTest, MissingRouteMeta) {
 
   std::string status;
 
-  EXPECT_CALL(filter_callbacks_, encodeHeaders_(_,_)).WillOnce(Invoke([&](HeaderMap& headers, bool) {
-          status = headers.Status()->value().c_str();
-    }));
-        
+  EXPECT_CALL(filter_callbacks_, encodeHeaders_(_, _))
+      .WillOnce(Invoke([&](HeaderMap &headers, bool) {
+        status = headers.Status()->value().c_str();
+      }));
 
   Envoy::Http::TestHeaderMapImpl headers{{":method", "GET"},
-                                        {":authority", "www.solo.io"},
-                                        {":path", "/getsomething"}};
+                                         {":authority", "www.solo.io"},
+                                         {":path", "/getsomething"}};
   filter_->decodeHeaders(headers, true);
 
   // test that we needed errored
@@ -183,5 +182,5 @@ TEST_F(FuncitonFilterTest, MissingRouteMeta) {
   EXPECT_FALSE(filter_->functionDecodeTrailersCalled_);
 }
 
-}
+} // namespace Http
 } // namespace Envoy
