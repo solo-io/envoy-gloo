0d76337cdc12ae3be152b4bc08c904c907ee1679
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-02-21 11:10:29 -0500

    adding filter test

--- test/common/http/filter/transformation_filter_test.cc
+++ test/common/http/filter/transformation_filter_test.cc
@@ -1,4 +1,5 @@
 #include "common/http/filter/transformation_filter.h"
+#include "common/config/transformation_well_known_names.h"
 
 #include "server/config/http/transformation_filter_config_factory.h"
 
@@ -31,6 +32,63 @@ TEST(TransformationFilterConfigFactory, EmptyConfig) {
 
   // shouldnt throw.
   TransformationFilterConfig cfg(config);
+  EXPECT_TRUE(cfg.empty());
+
+}
+
+
+class TransformationFilterTest : public testing::Test {
+public:
+
+  void SetUp() override {
+
+    initFilter();
+    Router::MockRouteEntry &routerentry =
+        filter_callbacks_.route_->route_entry_;
+    ON_CALL(routerentry, metadata()).WillByDefault(ReturnRef(route_metadata_));
+
+  }
+
+  void initFilter() {
+  Envoy::Http::TransformationFilterConfigSharedPtr configptr(new TransformationFilterConfig(config_));
+    filter_ = std::make_unique<TransformationFilter>(configptr);
+    filter_->setDecoderFilterCallbacks(filter_callbacks_);
+  }
+
+  void addNameToRoute(std::string name) {
+
+    auto& mymeta = *(*route_metadata_.mutable_filter_metadata())[Config::TransformationMetadataFilters::get().TRANSFORMATION].mutable_fields();
+        mymeta[Config::MetadataTransformationKeys::get().TRANSFORMATION]
+                .set_string_value(name);
+  }
+
+  envoy::api::v2::filter::http::Transformations config_;
+  Envoy::Http::TestHeaderMapImpl headers_{{":method", "GET"},
+                                         {":authority", "www.solo.io"},
+                                         {":path", "/path"}};
+
+  NiceMock<Envoy::Http::MockStreamDecoderFilterCallbacks> filter_callbacks_;
+  std::unique_ptr<TransformationFilter> filter_;
+  envoy::api::v2::core::Metadata route_metadata_;
+  
+};
+
+TEST_F(TransformationFilterTest, EmptyConfig) {
+  auto res = filter_->decodeHeaders(headers_, true);
+  EXPECT_EQ(FilterHeadersStatus::Continue, res);
+}
+
+TEST_F(TransformationFilterTest, StopIterationWhenNeedsToTransforms) {
+  auto& transformation = (*config_.mutable_transformations())["abc"];
+  transformation.mutable_request_template()->mutable_body()->set_text("solo");
+  initFilter(); // Re-load config.
+
+  addNameToRoute("abc");
+
+  
+  EXPECT_CALL(filter_callbacks_, addDecodedData(_, false)).Times(1);
+  auto res = filter_->decodeHeaders(headers_, true);
+  EXPECT_EQ(FilterHeadersStatus::Continue, res);
 }
 
 } // namespace Http
