a9cd12c815cca09d3f1d35314568cb6c362bd04d
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-02-20 22:51:45 -0500

    extract body

--- source/common/http/filter/transformer.cc
+++ source/common/http/filter/transformer.cc
@@ -1,6 +1,6 @@
 #include "common/http/filter/transformer.h"
 
-#include <iterator> 
+#include <iterator>
 
 #include "common/common/macros.h"
 
@@ -15,42 +15,42 @@ namespace Http {
 // TODO: move to common
 const Envoy::Http::HeaderEntry *getHeader(const HeaderMap &header_map,
                                           std::string &&key) {
-    auto lowerkey = LowerCaseString(std::move(key), true);
+  auto lowerkey = LowerCaseString(std::move(key), true);
 
-    const Envoy::Http::HeaderEntry *header_entry = header_map.get(lowerkey);
+  const Envoy::Http::HeaderEntry *header_entry = header_map.get(lowerkey);
   if (!header_entry) {
-        header_map.lookup(lowerkey, &header_entry);
-    }
-    return header_entry;
+    header_map.lookup(lowerkey, &header_entry);
+  }
+  return header_entry;
 }
 
 std::string ExtractorUtil::extract(
     const envoy::api::v2::filter::http::Extraction &extractor,
     const HeaderMap &header_map) {
-    std::string headername = extractor.header();
+  std::string headername = extractor.header();
   const Envoy::Http::HeaderEntry *header_entry =
       getHeader(header_map, std::move(headername));
   if (!header_entry) {
-        return "";
-    }
-
-    std::string value = header_entry->value().c_str();
-    unsigned int group = extractor.subgroup();
-    // get and regex
-    std::regex extract_regex(extractor.regex());
-    std::smatch regex_result;
-    if (std::regex_match(value, regex_result, extract_regex)) {
-        std::smatch::iterator submatch_it = regex_result.begin();
-        for (unsigned i = 0; i < group; i++) {
-            std::advance(submatch_it, 1);
-            if (submatch_it == regex_result.end()) {
-                return "";
-            }
-        }
-        return *submatch_it;
-    }
-
     return "";
+  }
+
+  std::string value = header_entry->value().c_str();
+  unsigned int group = extractor.subgroup();
+  // get and regex
+  std::regex extract_regex(extractor.regex());
+  std::smatch regex_result;
+  if (std::regex_match(value, regex_result, extract_regex)) {
+    std::smatch::iterator submatch_it = regex_result.begin();
+    for (unsigned i = 0; i < group; i++) {
+      std::advance(submatch_it, 1);
+      if (submatch_it == regex_result.end()) {
+        return "";
+      }
+    }
+    return *submatch_it;
+  }
+
+  return "";
 }
 
 TransformerInstance::TransformerInstance(
@@ -66,27 +66,27 @@ TransformerInstance::TransformerInstance(
 }
 
 json TransformerInstance::header_callback(Parsed::Arguments args, json data) {
-    std::string headername = env_.get_argument<std::string>(args, 0, data);
+  std::string headername = env_.get_argument<std::string>(args, 0, data);
   const Envoy::Http::HeaderEntry *header_entry =
       getHeader(header_map_, std::move(headername));
   if (!header_entry) {
-        return "";
-    }
-    return header_entry->value().c_str();
+    return "";
+  }
+  return header_entry->value().c_str();
 }
 
 json TransformerInstance::extracted_callback(Parsed::Arguments args,
                                              json data) {
-    std::string name = env_.get_argument<std::string>(args, 0, data);
+  std::string name = env_.get_argument<std::string>(args, 0, data);
   const auto value_it = extractions_.find(name);
-    if (value_it != extractions_.end()) {
-        return value_it->second;
-    }
-    return "";
+  if (value_it != extractions_.end()) {
+    return value_it->second;
+  }
+  return "";
 }
 
 std::string TransformerInstance::render(std::string input) {
-    return env_.render(input, context_);
+  return env_.render(input, context_);
 }
 
 Transformer::Transformer(
@@ -103,34 +103,32 @@ void transform(HeaderMap &hedader_map, Buffer::Instance &body) {
   std::string bodystring;
   bodystring.reserve(body.length());
 
-Transformer::Transformer(const envoy::api::v2::filter::http::Transformation& transformation): transformation_(transformation) {
+  uint64_t num_slices = body.getRawSlices(nullptr, 0);
+  Buffer::RawSlice slices[num_slices];
+  body.getRawSlices(slices, num_slices);
+
+  for (Buffer::RawSlice &slice : slices) {
+    const char *slice_mem = static_cast<const char *>(slice.mem_);
+    bodystring.append(slice_mem, slice.len_);
+  }
+
+  json json_body = json::parse(bodystring);
+
+  /*
+  TODO:
+      body to string
+      body string to json
+
+      for ever extraction,
+       extract it and add it to the map
+
+      For ever header in the template:
+       run it through the extraction, and insert them to the header map;
+
+      run the body through the extraction and update the incoming buffer
+      (i.e. drain and move)
+  */
 }
-
-Transformer::~Transformer(){
-}
-
-void transform(HeaderMap& hedader_map, Buffer::Instance &body) {
-    UNREFERENCED_PARAMETER(hedader_map);
-    UNREFERENCED_PARAMETER(body);
-    /*
-    TODO:
-        body to string
-        body string to json
-
-        for ever extraction,
-         extract it and add it to the map
-
-        For ever header in the template:
-         run it through the extraction, and insert them to the header map;
-
-        run the body through the extraction and update the incoming buffer 
-        (i.e. drain and move)
-    */
-
-}
-
-
-
 
 } // namespace Http
 } // namespace Envoy
