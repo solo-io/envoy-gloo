96883c9fe90f212789422c0b7bb81b540d86eaca
Tal Nordan <tal.nordan@solo.io>
2018-06-05 16:33:47 -0700

    Introduce e2e test

---
+++ e2e/create_config.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+
+set -e
+
+# prepare envoy config file.
+
+cat > envoy.yaml << EOF
+admin:
+  access_log_path: /dev/stdout
+  address:
+    socket_address:
+      address: 127.0.0.1
+      port_value: 19000
+static_resources:
+  listeners:
+  - name: listener_0
+    address:
+      socket_address: { address: 127.0.0.1, port_value: 10000 }
+    filter_chains:
+    - filters:
+      - name: envoy.http_connection_manager
+        config:
+          stat_prefix: http
+          codec_type: AUTO
+          route_config:
+            name: local_route
+            virtual_hosts:
+            - name: local_service
+              domains: ["*"]
+              routes:
+              - match:
+                  prefix: /get
+                route:
+                  cluster: cluster_0
+          http_filters:
+          - name: envoy.router
+      tls_context:
+        common_tls_context:
+          tls_certificates:
+          - certificate_chain:
+              filename: /tmp/pki/root/certs/www.acme.com.crt
+            private_key:
+              filename: /tmp/pki/root/keys/www.acme.com.key
+          tls_params: {}
+  clusters:
+  - connect_timeout: 5.000s
+    hosts:
+    - socket_address:
+        address: 127.0.0.1
+        port_value: 4222
+    name: cluster_0
+    type: STRICT_DNS
+EOF
---
+++ e2e/e2e_test.py
@@ -0,0 +1,83 @@
+import httplib
+import logging
+import os
+import requests
+import signal
+import subprocess
+import time
+import unittest
+
+
+def envoy_preexec_fn():
+  import ctypes
+  PR_SET_PDEATHSIG = 1  # See prtcl(2).
+  os.setpgrp()
+  libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
+  libc.prctl(PR_SET_PDEATHSIG, signal.SIGTERM)
+
+class ClientCertificateRestrictionTestCase(unittest.TestCase):
+  def setUp(self):
+    self.cleanup()
+
+  def tearDown(self):
+    if self.upstream is not None:
+      self.upstream.terminate()
+    if self.envoy is not None:
+      self.envoy.send_signal(signal.SIGINT)
+      self.envoy.wait()
+
+    self.cleanup()
+
+  def cleanup(self):
+    self.upstream = None
+    self.envoy = None
+
+  def __create_config(self):
+    for create_config_path in ("./create_config.sh", "./e2e/create_config.sh"):
+      if os.path.isfile(create_config_path):
+        subprocess.check_call(create_config_path)
+        break
+    else:
+      self.fail('"create_config.sh" was not found')
+
+  def __start_upstream(self):
+    for upstream_path in ("./upstream.py", "./e2e/upstream.py"):
+      if os.path.isfile(upstream_path):
+        self.upstream = subprocess.Popen([upstream_path])
+        break
+    else:
+      self.fail('"upstream.py" was not found')
+
+  def __start_envoy(self, prefix = None, suffix = None):
+    if prefix is None:
+      prefix = []
+    if suffix is None:
+      suffix = suffix = ["--log-level", "debug"] if DEBUG else []
+
+    envoy = os.environ.get("TEST_ENVOY_BIN","envoy")
+
+    self.envoy = subprocess.Popen(prefix + [envoy, "-c", "./envoy.yaml"]+suffix, preexec_fn=envoy_preexec_fn)
+    time.sleep(5)
+
+  def __make_request(self, expected_status):
+    # TODO(talnordan): Test client certificate validation.
+    response = requests.get('https://localhost:10000/get', verify=False)
+    print response
+    self.assertEqual(expected_status, response.status_code)
+    return response.text
+
+  def test_make_requests(self):
+    # Set up environment.
+    self.__create_config()
+    self.__start_upstream()
+    self.__start_envoy()
+
+    # Make a request.
+    response_text = self.__make_request(httplib.OK)
+
+if __name__ == "__main__":
+  global DEBUG
+  DEBUG =  True if os.environ.get("DEBUG","") != "0" else False
+  if DEBUG:
+    logging.basicConfig(level=logging.DEBUG)
+  unittest.main()
