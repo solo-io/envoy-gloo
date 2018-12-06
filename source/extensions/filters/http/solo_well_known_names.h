738b6807b2248046f94538484a00a4f45b3960e5
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-12-06 14:06:50 -0500

    cleanup

--- source/extensions/filters/http/solo_well_known_names.h
+++ source/extensions/filters/http/solo_well_known_names.h
@@ -20,12 +20,6 @@ public:
   // AWS Lambda filter
   // TODO(talnordan): Consider "io.solo.filters.http.aws_lambda".
   const std::string AwsLambda = "io.solo.aws_lambda";
-  // Cache filter
-  // TODO(talnordan): Consider "io.solo.filters.http.cache".
-  const std::string Cache = "io.solo.cache";
-  // JSON-gRPC transcoder filter
-  const std::string JsonGrpcTranscoder =
-      "io.solo.filters.http.json_grpc_transcoder";
   // NATS Streaming filter
   // TODO(talnordan): Consider "io.solo.filters.http.nats_streaming".
   const std::string NatsStreaming = "io.solo.nats_streaming";
