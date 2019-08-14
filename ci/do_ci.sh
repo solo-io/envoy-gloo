eb75b42640f92adb781818fc3c06ea4e96ca9ddd
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-08-13 20:17:07 -0400

    update (#18)
    * update
    * workspace
    * update
    * workspace
    * build container
    * deps
    * update setup_clang_toolchain
    * give bazel some room
    * update bazel opts
    * bazelin'
    * try first building
    * coverage seems hard
    * update

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -17,11 +17,11 @@ fi
 ENVOY_SRCDIR=$PWD
 ENVOY_CIDIR=${ENVOY_SRCDIR}/ci
 # replace bazel cache to be in our volume
+export USER=bazel
 export TEST_TMPDIR=${BUILD_DIR}/tmp
-export USER=root
+export BAZEL="bazel"
 
-BAZEL_OPTIONS="--package_path %workspace%:${ENVOY_SRCDIR}"
-
+export PPROF_PATH=/thirdparty_build/bin/pprof
 [ -z "${NUM_CPUS}" ] && NUM_CPUS=`grep -c ^processor /proc/cpuinfo`
 
 # Create a fake home. Python site libs tries to do getpwuid(3) if we don't and the CI
@@ -32,30 +32,58 @@ mkdir -p "${FAKE_HOME}"
 export HOME="${FAKE_HOME}"
 export PYTHONUSERBASE="${FAKE_HOME}"
 
-export BAZEL_BUILD_OPTIONS="--strategy=Genrule=standalone --genrule_strategy=standalone \
-  --spawn_strategy=standalone \
-  --verbose_failures ${BAZEL_OPTIONS} --action_env=HOME --action_env=PYTHONUSERBASE \
-  --jobs=${NUM_CPUS} --show_task_finish ${BAZEL_BUILD_EXTRA_OPTIONS}"
-
-export BAZEL_TEST_OPTIONS="${BAZEL_BUILD_OPTIONS} --test_env=HOME --test_env=PYTHONUSERBASE \
-  --test_env=UBSAN_OPTIONS=print_stacktrace=1 \
-  --cache_test_results=no --test_output=all ${BAZEL_EXTRA_TEST_OPTIONS}"
+if [[ -f "/etc/redhat-release" ]]
+then
+  export BAZEL_BUILD_EXTRA_OPTIONS="--copt=-DENVOY_IGNORE_GLIBCXX_USE_CXX11_ABI_ERROR=1 --action_env=PATH ${BAZEL_BUILD_EXTRA_OPTIONS}"
+else
+  export BAZEL_BUILD_EXTRA_OPTIONS="--action_env=PATH=/bin:/usr/bin:/usr/lib/llvm-8/bin --linkopt=-fuse-ld=lld ${BAZEL_BUILD_EXTRA_OPTIONS}"
+fi
 
 # added by yuval-k for the integration tests to run on google cloud build
-export BAZEL_TEST_OPTIONS="${BAZEL_TEST_OPTIONS} --test_env=ENVOY_IP_TEST_VERSIONS=v4only --test_output=errors"
+export BAZEL_EXTRA_TEST_OPTIONS="--test_env=ENVOY_IP_TEST_VERSIONS=v4only --test_output=errors"
+
+export BAZEL_QUERY_OPTIONS="${BAZEL_OPTIONS}"
+export BAZEL_BUILD_OPTIONS="--verbose_failures ${BAZEL_OPTIONS} --action_env=HOME --action_env=PYTHONUSERBASE \
+  --local_cpu_resources=${NUM_CPUS} --show_task_finish --experimental_generate_json_trace_profile \
+  --test_env=HOME --test_env=PYTHONUSERBASE --cache_test_results=no --test_output=all \
+  ${BAZEL_BUILD_EXTRA_OPTIONS} ${BAZEL_EXTRA_TEST_OPTIONS}"
 
 function setup_gcc_toolchain() {
-  export CC=gcc
-  export CXX=g++
-  echo "$CC/$CXX toolchain configured"
+  if [[ -z "${ENVOY_RBE}" ]]; then
+    export CC=gcc
+    export CXX=g++
+    export BAZEL_COMPILER=gcc
+    echo "$CC/$CXX toolchain configured"
+  else
+    export BAZEL_BUILD_OPTIONS="--config=rbe-toolchain-gcc ${BAZEL_BUILD_OPTIONS}"
+  fi
 }
 
 function setup_clang_toolchain() {
-  export PATH=/usr/lib/llvm-8/bin:$PATH
-  export CC=clang
-  export CXX=clang++
-  export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-8/bin/llvm-symbolizer
-  echo "$CC/$CXX toolchain configured"
+  if [[ -z "${ENVOY_RBE}" ]]; then
+    export PATH=/usr/lib/llvm-8/bin:$PATH
+    export CC=clang
+    export CXX=clang++
+    export BAZEL_COMPILER=clang
+    export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-8/bin/llvm-symbolizer
+    echo "$CC/$CXX toolchain configured"
+  else
+    export BAZEL_BUILD_OPTIONS="--config=rbe-toolchain-clang ${BAZEL_BUILD_OPTIONS}"
+  fi
+}
+
+function setup_clang_libcxx_toolchain() {
+  if [[ -z "${ENVOY_RBE}" ]]; then
+    export PATH=/usr/lib/llvm-8/bin:$PATH
+    export CC=clang
+    export CXX=clang++
+    export BAZEL_COMPILER=clang
+    export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-8/bin/llvm-symbolizer
+    export BAZEL_BUILD_OPTIONS="--config=libc++ ${BAZEL_BUILD_OPTIONS}"
+    echo "$CC/$CXX toolchain with libc++ configured"
+  else
+    export BAZEL_BUILD_OPTIONS="--config=rbe-toolchain-clang-libc++ ${BAZEL_BUILD_OPTIONS}"
+  fi
 }
 
 function cleanup() {
@@ -79,7 +107,9 @@ case "$1" in
     ${ENVOY_SRCDIR}test/run_envoy_bazel_coverage.sh
     ;;
 "test")
-    bazel test ${BAZEL_TEST_OPTIONS} -c opt //test/...
+     bazel test ${BAZEL_BUILD_OPTIONS} -c opt //test/extensions/...
+#    bazel build ${BAZEL_BUILD_OPTIONS} -c opt //test/...
+#    bazel test ${BAZEL_BUILD_OPTIONS} -c opt //test/...
     ;;
 "build")
     bazel build ${BAZEL_BUILD_OPTIONS} -c opt :envoy
