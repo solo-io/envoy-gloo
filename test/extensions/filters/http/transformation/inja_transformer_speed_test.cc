28bd9cb06cbd42c7340fc7b8ec0d1aaad196b42e
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-10-04 08:38:09 -0400

    speed test

---
+++ test/extensions/filters/http/transformation/inja_transformer_speed_test.cc
@@ -0,0 +1,47 @@
+#include "common/common/empty_string.h"
+
+#include "extensions/filters/http/transformation/inja_transformer.h"
+
+#include "test/test_common/utility.h"
+
+#include "benchmark/benchmark.h"
+#include "fmt/format.h"
+
+using json = nlohmann::json;
+
+namespace Envoy {
+namespace Extensions {
+namespace HttpFilters {
+namespace Transformation {
+
+namespace {
+std::function<const std::string &()> empty_body = [] { return EMPTY_STRING; };
+}
+
+static void BM_ExrtactHeader(benchmark::State &state) {
+  Http::TestHeaderMapImpl headers{{":method", "GET"},
+                                  {":authority", "www.solo.io"},
+                                  {":path", "/users/123"}};
+  envoy::api::v2::filter::http::Extraction extractor;
+  extractor.set_header(":path");
+  extractor.set_regex("/users/(\\d+)");
+  extractor.set_subgroup(1);
+  size_t output_bytes = 0;
+
+  Extractor ext(extractor);
+  for (auto _ : state) {
+    auto view = ext.extract(headers, empty_body);
+    output_bytes += view.length();
+  }
+  benchmark::DoNotOptimize(output_bytes);
+}
+// Register the function as a benchmark
+BENCHMARK(BM_ExrtactHeader);
+
+} // namespace Transformation
+} // namespace HttpFilters
+} // namespace Extensions
+} // namespace Envoy
+
+// Run the benchmark
+BENCHMARK_MAIN();
