e007b736fb1dd44fda84727ca4976ecad20cfd1c
Yuval Kohavi <yuval.kohavi@gmail.com>
2020-07-01 14:41:37 +0000

    16 jobs in CI?

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -27,5 +27,8 @@ export ENVOY_SRCDIR=$SOURCE_DIR
 # google cloud build doesn't like ipv6
 export BAZEL_EXTRA_TEST_OPTIONS="--test_env=ENVOY_IP_TEST_VERSIONS=v4only --test_output=errors"
 
+# google cloud build times out when using full throttle.
+export NUM_CPUS=16
+
 echo Building
 bash -x $UPSTREAM_ENVOY_SRCDIR/ci/do_ci.sh "$@"
