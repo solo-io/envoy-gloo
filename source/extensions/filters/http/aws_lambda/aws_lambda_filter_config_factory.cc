2bb5a3c08828184673f98e3606b149a0af3c9de2
EItanya <eitan.yarmush@solo.io>
2020-07-30 11:50:44 -0500

    almost compiling

--- source/extensions/filters/http/aws_lambda/aws_lambda_filter_config_factory.cc
+++ source/extensions/filters/http/aws_lambda/aws_lambda_filter_config_factory.cc
@@ -21,9 +21,9 @@ AWSLambdaFilterConfigFactory::createFilterFactoryFromProtoTyped(
   auto config = std::make_shared<AWSLambdaConfigImpl>(
       std::make_unique<
           Extensions::Common::Aws::DefaultCredentialsProviderChain>(
-          context.api(), Extensions::Common::Aws::Utility::metadataFetcher),
-      context.dispatcher(), context.threadLocal(), stats_prefix,
-      context.scope(), proto_config);
+          context.api(), Extensions::Common::Aws::Utility::metadataFetcher), context.clusterManager(),
+      context.dispatcher(), context.threadLocal(), stats_prefix, context.scope(), context.api(), 
+      proto_config);
 
   return [&context,
           config](Http::FilterChainFactoryCallbacks &callbacks) -> void {
