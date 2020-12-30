6637c63bf2530fafdb61d1e14bfbe599d2e56de4
Shane O'Donnell <shane.odonnell@solo.io>
2020-12-30 15:33:25 -0500

    More imports

--- test/extensions/filters/http/transformation/inja_transformer_test.cc
+++ test/extensions/filters/http/transformation/inja_transformer_test.cc
@@ -3,6 +3,7 @@
 
 #include "test/mocks/common.h"
 #include "test/mocks/http/mocks.h"
+#include "common/matcher/matcher.h"
 #include "test/mocks/server/mocks.h"
 #include "test/mocks/upstream/mocks.h"
 #include "test/test_common/environment.h"
