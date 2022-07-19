f297dc863f9bff7e2acfae34a1170fc1de98ac6a
Ben Taussig <ben.taussig@solo.io>
2022-07-19 17:19:43 +0000

    fix api_gateway tests

--- test/extensions/filters/http/aws_lambda/api_gateway_transformer_test.cc
+++ test/extensions/filters/http/aws_lambda/api_gateway_transformer_test.cc
@@ -238,18 +238,15 @@ TEST(ApiGatewayTransformer, error) {
                                          {":authority", "www.solo.io"},
                                          {"x-test", "789"},
                                          {":path", "/users/123"}};
-  Http::TestRequestHeaderMapImpl request_headers{{":method", "GET"},
-                                         {":authority", "www.solo.io"},
-                                         {"x-test", "789"},
-                                         {":path", "/users/123"}};
+  Http::TestResponseHeaderMapImpl response_headers{};
   Buffer::OwnedImpl body("{invalid json}");
   ApiGatewayTransformer transformer;
   NiceMock<Http::MockStreamDecoderFilterCallbacks> filter_callbacks_{};
-  EXPECT_ANY_THROW(transformer.transform(request_headers, &headers, body, filter_callbacks_));
+  transformer.transform(response_headers, &headers, body, filter_callbacks_);
 
-  EXPECT_EQ(response_headers.getStatusValue(), "500");
-  EXPECT_EQ(response_headers.get(Http::LowerCaseString("content-type")), "text/plain");
-  EXPECT_EQ(response_headers.get(Http::LowerCaseString("x-amzn-errortype")), "500");
+  EXPECT_EQ(response_headers.getStatusValue(), "400");
+  EXPECT_EQ(response_headers.get(Http::LowerCaseString("content-type"))[0]->value().getStringView(), "text/plain");
+  EXPECT_EQ(response_headers.get(Http::LowerCaseString("x-amzn-errortype"))[0]->value().getStringView(), "400");
 }
 
 } // namespace AwsLambda
