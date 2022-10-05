c6033c41475a2c7b7e7985a36fc9ac0763358ed5
Ben Taussig <85883594+ben-taussig-solo@users.noreply.github.com>
2022-10-05 10:38:29 -0400

    Release-v1.21.3-patchX: Backport Base64 and Substring Transformation Functions (#184) (#185)
    * Create Base64 and Substring Transformation Functions (#184)
    
    * create base64encode and base64decode inja functions
    
    * add testing
    
    * add changelog entry
    
    * remove extraneous comments
    
    * add support for substring function
    
    * use snake_case for base64_encode and base64_decode callbacck names
    
    * update substring function to return entire remaining substring on non-positive substring_len argument
    
    * suppoort calling substring method with two arguments
    
    * update changelog
    
    * add handling for non-int arguments in substring function
    
    * add test against invalid base64 decoding
    
    * clarify argument handling commentary in substring_callback
    
    * convert changelog to NON_USER_FACING to work around issue with /skip-changelog

--- source/extensions/filters/http/transformation/inja_transformer.cc
+++ source/extensions/filters/http/transformation/inja_transformer.cc
@@ -126,6 +126,22 @@ TransformerInstance::TransformerInstance(
   env_.add_callback("clusterMetadata", 1, [this](Arguments &args) {
     return cluster_metadata_callback(args);
   });
+  env_.add_callback("base64_encode", 1, [this](Arguments &args) {
+    return base64_encode_callback(args); 
+  });
+  env_.add_callback("base64_decode", 1, [this](Arguments &args) {
+    return base64_decode_callback(args); 
+  });
+  // substring can be called with either two or three arguments --
+  // the first argument is the string to be modified, the second is the start position
+  // of the substring, and the optional third argument is the length of the substring.
+  // If the third argument is not provided, the substring will extend to the end of the string.
+  env_.add_callback("substring", 2, [this](Arguments &args) {
+    return substring_callback(args); 
+  });
+  env_.add_callback("substring", 3, [this](Arguments &args) {
+    return substring_callback(args); 
+  });
 }
 
 json TransformerInstance::header_callback(const inja::Arguments &args) const {
@@ -242,6 +258,59 @@ json TransformerInstance::cluster_metadata_callback(
   }
   }
   return "";
+}
+
+json TransformerInstance::base64_encode_callback(const inja::Arguments &args) const {
+  const std::string &input = args.at(0)->get_ref<const std::string &>();
+  return Base64::encode(input.c_str(), input.length());
+}
+
+json TransformerInstance::base64_decode_callback(const inja::Arguments &args) const {
+  const std::string &input = args.at(0)->get_ref<const std::string &>();
+  return Base64::decode(input);
+}
+
+// return a substring of the input string, starting at the start position
+// and extending for length characters. If length is not provided, the
+// substring will extend to the end of the string.
+json TransformerInstance::substring_callback(const inja::Arguments &args) const {
+  // get first argument, which is the string to be modified
+  const std::string &input = args.at(0)->get_ref<const std::string &>();
+  
+  // try to get second argument (start position) as an int64_t
+  int start = 0;
+  try {
+    start = args.at(1)->get_ref<const int64_t &>();
+  } catch (const std::exception &e) {
+    // if it can't be converted to an int64_t, return an empty string
+    return "";
+  }
+
+  // try to get optional third argument (length) as an int64_t
+  int64_t substring_len = -1;
+  if (args.size() == 3) {
+    try {
+      substring_len = args.at(2)->get_ref<const int64_t &>();
+    } catch (const std::exception &e) {
+      // if it can't be converted to an int64_t, return an empty string
+      return "";
+    }
+  }
+  const int64_t input_len = input.length();
+
+  // if start is negative, or start is greater than the length of the string, return empty string
+  if (start < 0 ||  start >= input_len) {
+    return "";
+  }
+
+  // if supplied substring_len is <= 0 or start + substring_len is greater than the length of the string, 
+  // return the substring from start to the end of the string
+  if (substring_len <= 0 || start + substring_len > input_len) {
+    return input.substr(start);
+  }
+
+  // otherwise, return the substring from start to start + len
+  return input.substr(start, substring_len);
 }
 
 std::string TransformerInstance::render(const inja::Template &input) {
