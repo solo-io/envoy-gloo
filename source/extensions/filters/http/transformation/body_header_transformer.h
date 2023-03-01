7d220a00255742aa470a5f6dad8464d460ee4937
ben-taussig-solo <ben.taussig@solo.io>
2023-03-01 20:09:16 +0000

    filter changes to support new fields in requestTransformation

--- source/extensions/filters/http/transformation/body_header_transformer.h
+++ source/extensions/filters/http/transformation/body_header_transformer.h
@@ -17,11 +17,18 @@ public:
                  Buffer::Instance &body,
                  Http::StreamFilterCallbacks &) const override;
   bool passthrough_body() const override { return false; };
-
+  void parse_headers(const Http::RequestOrResponseHeaderMap &header_map,
+                     std::map<std::string, absl::string_view> &headers,
+                     std::map<std::string, std::vector<absl::string_view>> &multi_value_headers) const;
+  void parse_query_string(absl::string_view query_string,
+                        std::map<std::string, std::string> &query_string_parameters,
+                        std::map<std::string, std::vector<std::string>> &multi_value_query_string_parameters) const;
 private:
   bool add_request_metadata_{};
 
 };
+
+Envoy::Http::Utility::QueryParamsVector parse_parameters(absl::string_view data, size_t start);
 
 } // namespace Transformation
 } // namespace HttpFilters
