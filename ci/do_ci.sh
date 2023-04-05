49f5fdd5754b6963005d4f6cc26776ae60111d5d
Nathan Fudenberg <nathan.fudenberg@solo.io>
2023-04-05 12:55:29 -0400

    1.23/bazel: Bump for cve (#220)
    * bazel: Bump for cve and get a few other small things at the same time.
    
    * changelog: Reflect both updates
    
    * ci: Update to not break at looking at Version file
    
    * alpine bump
    
    ---------
    
    Co-authored-by: Gunnar <gunnar.horve@solo.io>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -46,5 +46,7 @@ export ENVOY_CONTRIB_BUILD_DEBUG_INFORMATION="//source/exe:envoy-static.dwp"
 echo Building
 sed -i "s|//test/tools/schema_validator:schema_validator_tool|@envoy//test/tools/schema_validator:schema_validator_tool|" "$UPSTREAM_ENVOY_SRCDIR/ci/do_ci.sh"
 sed -i "s|bazel-bin/test/tools/schema_validator/schema_validator_tool|bazel-bin/external/envoy/test/tools/schema_validator/schema_validator_tool|" "$UPSTREAM_ENVOY_SRCDIR/ci/do_ci.sh"
+sed -i "s|VERSION.txt|ci/FAKEVERSION.txt|" "$UPSTREAM_ENVOY_SRCDIR/ci/do_ci.sh"
+sed -i "s|\${ENVOY_SRCDIR}/VERSION.txt|ci/FAKEVERSION.txt|" "$UPSTREAM_ENVOY_SRCDIR/ci/build_setup.sh"
 
 bash -x $UPSTREAM_ENVOY_SRCDIR/ci/do_ci.sh "$@"
