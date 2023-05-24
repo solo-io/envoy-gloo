8e5912e240d4fc954fcc77c7927d5d1baf784260
Ben Taussig <ben.taussig@solo.io>
2023-05-24 15:12:57 +0000

    add transformation_logger

---
+++ source/extensions/filters/http/transformation/transformation_logger.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "source/common/common/logger.h"
+
+// TODO: remove logs to stdout
+#define TRANSFORMATION_LOG(LEVEL, FORMAT, STREAM, ...)                  \
+  do {                                                                  \
+      ENVOY_STREAM_LOG(LEVEL, FORMAT, STREAM, ##__VA_ARGS__);           \
+      std::cout << "REQUEST_RESPONSE_LOG: " << FORMAT << std::endl;     \
+  } while (0)
+
+#define TRANSFORMATION_LOG_IF(LEVEL, CONDITION, FORMAT, STREAM, ...)        \
+    do {                                                                    \
+        if (CONDITION) {                                                    \
+            TRANSFORMATION_LOG(LEVEL, FORMAT, STREAM, ##__VA_ARGS__);       \
+        }                                                                   \
+    } while (0)
+
+#define TRANSFORMATION_SENSITIVE_LOG(LEVEL, FORMAT, PARAMS, ...)                               \
+    do {                                                                                       \
+        TRANSFORMATION_LOG_IF(true, LEVEL, FORMAT, (PARAMS).stream_callbacks_, ##__VA_ARGS__); \
+    } while (0)
