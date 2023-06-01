306e7e5005233250e09b8d85584865037f73cd01
Ben Taussig <ben.taussig@solo.io>
2023-06-01 13:36:52 +0000

    update ApiGatewayTransformer constructor

--- source/extensions/transformers/aws_lambda/api_gateway_transformer.cc
+++ source/extensions/transformers/aws_lambda/api_gateway_transformer.cc
@@ -24,7 +24,7 @@ ApiGatewayTransformerFactory::createTransformer(
   return std::make_shared<ApiGatewayTransformer>();
 }
 
-ApiGatewayTransformer::ApiGatewayTransformer(){}
+ApiGatewayTransformer::ApiGatewayTransformer(): Transformation::Transformer(false) {}
 const Envoy::Http::LowerCaseString ApiGatewayTransformer::AMAZON_ERRORTYPE_HEADER = Envoy::Http::LowerCaseString("x-amzn-errortype");
 
 void ApiGatewayTransformer::format_error(
