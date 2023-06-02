6266b19845b85be9b71ab6329af9245345135d09
Ben Taussig <ben.taussig@solo.io>
2023-06-02 17:02:59 +0000

    mark log_request_response_info as unused in test transformers

--- test/extensions/filters/http/transformation/fake_transformer.h
+++ test/extensions/filters/http/transformation/fake_transformer.h
@@ -28,7 +28,7 @@ public:
   std::string name() const override {return "io.solo.transformer.fake";}
 
   TransformerConstSharedPtr createTransformer(const Protobuf::Message &,
-  google::protobuf::BoolValue log_request_response_info,
+   __attribute__((unused)) google::protobuf::BoolValue log_request_response_info,
   Server::Configuration::CommonFactoryContext &) override {
     return std::make_shared<FakeTransformer>();
   }
