40618a711289f7a475e538dec1de7ce84196761f
Jacob Bohanon <jacob.bohanon@solo.io>
2023-09-18 11:54:23 -0400

    extract binary

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -4,36 +4,23 @@ bazel fetch //source/exe:envoy-static
 
 SOURCE_DIR="$(bazel info workspace)"
 
+"$SOURCE_DIR/ci/verify_posture.sh" verify
 
-ENVOY_DEPENDENCY=$(awk '/envoy = dict/{p=1}; /commit/{print $3; exit}' bazel/repository_locations.bzl | sed -E 's|"([a-z0-9]+)",|\1|g')
-echo $ENVOY_DEPENDENCY
-
-# # will be reverted or updated in https://github.com/solo-io/envoy-gloo/issues/246
-# git clone https://github.com/envoyproxy/envoy.git /tmp/envoy
-# pushd /tmp/envoy
-# git remote add public https://github.com/solo-io/envoy-fork
-# git fetch --all
-# git checkout "$ENVOY_DEPENDENCY"
-# popd
-
-$SOURCE_DIR/ci/verify_posture.sh verify
-
-# export UPSTREAM_ENVOY_SRCDIR=/tmp/envoy
 export UPSTREAM_ENVOY_SRCDIR=$(bazel info output_base)/external/envoy
-cp -f $UPSTREAM_ENVOY_SRCDIR/.bazelrc $SOURCE_DIR/
-cp -f $UPSTREAM_ENVOY_SRCDIR/.bazelversion $SOURCE_DIR/.bazelversion
-cp -f $UPSTREAM_ENVOY_SRCDIR/ci/WORKSPACE.filter.example $SOURCE_DIR/ci/
-cp -f $UPSTREAM_ENVOY_SRCDIR/VERSION.txt $SOURCE_DIR/VERSION.txt
 
-cp -f $UPSTREAM_ENVOY_SRCDIR/tools/shell_utils.sh $SOURCE_DIR/tools
+cp -f "$UPSTREAM_ENVOY_SRCDIR/.bazelrc"                    "$SOURCE_DIR/"
+cp -f "$UPSTREAM_ENVOY_SRCDIR/.bazelversion"               "$SOURCE_DIR/.bazelversion"
+cp -f "$UPSTREAM_ENVOY_SRCDIR/ci/WORKSPACE.filter.example" "$SOURCE_DIR/ci/"
+cp -f "$UPSTREAM_ENVOY_SRCDIR/VERSION.txt"                 "$SOURCE_DIR/VERSION.txt"
+cp -f "$UPSTREAM_ENVOY_SRCDIR/tools/shell_utils.sh"        "$SOURCE_DIR/tools"
 
 
-if [ -f $UPSTREAM_ENVOY_SRCDIR/bazel/setup_clang.sh ]; then
-  cp $UPSTREAM_ENVOY_SRCDIR/bazel/setup_clang.sh bazel/
+if [ -f "$UPSTREAM_ENVOY_SRCDIR/bazel/setup_clang.sh" ]; then
+  cp "$UPSTREAM_ENVOY_SRCDIR/bazel/setup_clang.sh" bazel/
 fi
 
 if [ -n "$COMMIT_SHA" ]; then
-  echo $COMMIT_SHA > SOURCE_VERSION
+  echo "$COMMIT_SHA" > SOURCE_VERSION
 fi
 
 export ENVOY_SRCDIR=$SOURCE_DIR
@@ -58,14 +45,14 @@ echo "test $BUILD_CONFIG" >> "${SOURCE_DIR}/test.bazelrc"
 
 echo Building
 sed -i 's|"//contrib/..."||' "$UPSTREAM_ENVOY_SRCDIR/ci/do_ci.sh"
-# sed -i 's|//distribution|@envoy//distribution|' "$UPSTREAM_ENVOY_SRCDIR/ci/do_ci.sh"
-# sed -i 's|bazel-bin/distribution|bazel-bin/external/envoy/distribution|' "$UPSTREAM_ENVOY_SRCDIR/ci/do_ci.sh"
 
-bash -x $UPSTREAM_ENVOY_SRCDIR/ci/do_ci.sh "$@"
+bash -x "$UPSTREAM_ENVOY_SRCDIR/ci/do_ci.sh" "$@"
 
-bazel run @envoy//tools/ztsd -- --stdout -d /build/envoy/x64/bin/release.tar.zst \
-    | tar xfO - envoy > linux/amd64/build_envoy_release_stripped/envoy
+ENVOY_GLOO_BIN_DIR='linux/amd64/build_envoy_release_stripped'
+mkdir -p "$ENVOY_GLOO_BIN_DIR"
+bazel run @envoy//tools/zstd:zstd -- --stdout -d /build/envoy/x64/bin/release.tar.zst \
+    | tar xfO - envoy > "$ENVOY_GLOO_BIN_DIR/envoy"
 
-$SOURCE_DIR/ci/static_analysis.sh
+"$SOURCE_DIR/ci/static_analysis.sh"
 
 echo "CI completed"
