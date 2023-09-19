2be8f132372d6d5ce961d58478896ace15a269fe
Jacob Bohanon <jacob.bohanon@solo.io>
2023-09-19 13:06:28 -0400

    fix static analysis script

--- ci/static_analysis.sh
+++ ci/static_analysis.sh
@@ -15,13 +15,13 @@ tools/gen_compilation_database.py
 
 sed -i 's^ -fno-canonical-system-headers^^' compile_commands.json
 
-PATH=/opt/llvm/bin:$PATH python3 /opt/llvm/bin/analyze-build --cdb compile_commands.json --verbose -o /tmp/analysis --status-bugs \
+if ! PATH=/opt/llvm/bin:$PATH python3 /opt/llvm/bin/analyze-build --cdb compile_commands.json --verbose -o /tmp/analysis --status-bugs \
     --exclude $(bazel info execution_root)/test
-EXIT_CODE=$?
-if [ $EXIT_CODE != 0 ]; then
+then
+  echo "analysis failed; copying files"
   ANALYSIS_DIR='linux/amd64/analysis'
   mkdir -p "$ANALYSIS_DIR"
   chmod -R +r /tmp/analysis
   cp -r /tmp/analysis/* "$ANALYSIS_DIR"
-  exit "$EXIT_CODE"
+  exit 1
 fi
