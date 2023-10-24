5e678e86d9063e5a75034fdca5f6fd7b15dc24fc
Ben Taussig <ben.taussig@solo.io>
2023-10-24 13:51:43 +0000

    remove redefinitions

--- source/extensions/filters/http/transformation/transformation_factory.cc
+++ source/extensions/filters/http/transformation/transformation_factory.cc
@@ -15,7 +15,7 @@ TransformerConstSharedPtr Transformation::getTransformer(
   switch (transformation.transformation_type_case()) {
   case envoy::api::v2::filter::http::Transformation::kTransformationTemplate:
     return std::make_unique<InjaTransformer>(
-        transformation.transformation_template(), context.api().randomGenerator(), context.threadLocal());
+        transformation.transformation_template());
   case envoy::api::v2::filter::http::Transformation::kHeaderBodyTransform: {
     const auto& header_body_transform = transformation.header_body_transform();
     return std::make_unique<BodyHeaderTransformer>(header_body_transform.add_request_metadata());
