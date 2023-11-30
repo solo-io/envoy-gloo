a0e209303e340291888cc3cec5c600755d883144
Ben Taussig <ben.taussig@solo.io>
2023-11-30 15:39:23 -0500

    updates to script

--- ci/check_extensions_build_config.sh
+++ ci/check_extensions_build_config.sh
@@ -4,13 +4,21 @@
 ### Helper functions ###
 ########################
 
-# Define the upstream repository and file path
+# Define the upstream repository and file path to the upstream extensions_build_config.bzl
 UPSTREAM_REPO="envoyproxy/envoy"
 UPSTREAM_FILE_PATH="source/extensions/extensions_build_config.bzl"
-UPSTREAM_URL="https://raw.githubusercontent.com/$UPSTREAM_REPO/main/$UPSTREAM_FILE_PATH"
-
+# Define the path to the local repository_locations.bzl, which contains the envoy commit hash
+REPOSITORY_LOCATIONS_FILE="./bazel/repository_locations.bzl"
 # Define the path to the local version
 ENVOY_GLOO_FILE="./bazel/extensions/extensions_build_config.bzl"
+
+# Function to extract the envoy commit hash from repository_locations.bzl
+get_envoy_commit_hash() {
+    local file_path="$1"
+    local commit_hash
+    commit_hash=$(grep -A 2 "envoy =" "$file_path" | grep commit | cut -d '"' -f 2)
+    echo "$commit_hash"
+}
 
 # Function to trim leading and trailing whitespaces
 trim() {
@@ -24,7 +32,7 @@ trim() {
 report_error() {
     local line_number=$1
     local line_content="$2"
-    local github_line_url="https://github.com/$UPSTREAM_REPO/blob/main/$UPSTREAM_FILE_PATH#L$line_number"
+    local github_line_url="https://github.com/$UPSTREAM_REPO/blob/$ENVOY_COMMIT_HASH/$UPSTREAM_FILE_PATH#L$line_number"
     echo "Error: Line not found in the envoy-gloo version of extensions_build_config.bzl at line $line_number\n\t$line_content\n\tSee: $github_line_url"
 }
 
@@ -32,23 +40,30 @@ report_error() {
 #### Main execution ####
 ########################
 
+# Extract the envoy commit hash
+ENVOY_COMMIT_HASH=$(get_envoy_commit_hash "$REPOSITORY_LOCATIONS_FILE")
+if [ -z "$ENVOY_COMMIT_HASH" ]; then
+    echo "Error: Failed to extract envoy commit hash from $REPOSITORY_LOCATIONS_FILE"
+    exit 1
+fi
+
+# Update the URL to point to the specific envoy commit
+UPSTREAM_URL="https://raw.githubusercontent.com/$UPSTREAM_REPO/$ENVOY_COMMIT_HASH/$UPSTREAM_FILE_PATH"
+
 # Initialize arrays to store the upstream and envoy-gloo versions of the file
 declare -a UPSTREAM_LINES ENVOY_GLOO_LINES
 
-# Read the upstream version of the file into an array
-curl -s "$UPSTREAM_URL" -o upstream_file.tmp
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to fetch the upstream file from $UPSTREAM_URL"
-    exit 1
-fi
-IFS=$'\n' read -r -d '' -a UPSTREAM_LINES < upstream_file.tmp
+# Read the upstream version of the file into an array, including empty lines
+UPSTREAM_LINES=()
+while IFS= read -r line || [[ -n "$line" ]]; do
+    UPSTREAM_LINES+=("$line")
+done < upstream_file.tmp
 
-# Check if the envoy-gloo file exists and is readable
-if [ ! -r "$ENVOY_GLOO_FILE" ]; then
-    echo "Error: The envoy-gloo file $ENVOY_GLOO_FILE does not exist or is not readable"
-    exit 1
-fi
-IFS=$'\n' read -r -d '' -a ENVOY_GLOO_LINES < "$ENVOY_GLOO_FILE"
+# Read the envoy-gloo version of the file into an array, including empty lines
+ENVOY_GLOO_LINES=()
+while IFS= read -r line || [[ -n "$line" ]]; do
+    ENVOY_GLOO_LINES+=("$line")
+done < "$ENVOY_GLOO_FILE"
 
 # Preprocess the envoy-gloo lines for faster lookup
 declare -a PROCESSED_ENVOY_GLOO_LINES
