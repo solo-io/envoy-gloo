831067badda8c957667386e4688c03b20a284b90
Ashish Banerjee <ashish.banerjee@solo.io>
2024-05-13 18:33:16 +0000

    Add comments explaining why `//test/tools` exist

--- test/tools/router_check/router_check.cc
+++ test/tools/router_check/router_check.cc
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 
+// This file only exists to get upstream's `do_ci.sh` to pass, as it is
+// hard-coded to build this target
 
 int main(int, char**) {
   return EXIT_SUCCESS;
