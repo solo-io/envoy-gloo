651d86f7e58a97bb1866e9a179b4cac863a54deb
ashish b <108897222+ashishb-solo@users.noreply.github.com>
2024-10-01 13:26:51 -0400

    Set opentelemetry span name in transformation filter (#368)
    * Add setOperation patch
    
    * Add changelog
    
    * Transformation initial implementation
    
    Should work but does not seem to at the moment
    
    * Implement unit test for setting span name
    
    * Update changelog
    
    * Remove extra entry from changelog
    
    * Do not override route.decorator.operation
    
    * Add test for null route in callbacks
    
    * Remove redundant call to setOperation
    
    This is the default value that is set from the HCM - we do not need to write it
    a second time.

--- test/extensions/filters/http/transformation/inja_transformer_test.cc
+++ test/extensions/filters/http/transformation/inja_transformer_test.cc
@@ -7,6 +7,7 @@
 #include "test/mocks/http/mocks.h"
 #include "test/mocks/server/mocks.h"
 #include "test/mocks/thread_local/mocks.h"
+#include "test/mocks/tracing/mocks.h"
 #include "test/mocks/upstream/mocks.h"
 #include "test/test_common/environment.h"
 
@@ -1237,6 +1238,85 @@ TEST_F(InjaTransformerTest, ParseFromClusterMetadata) {
   EXPECT_EQ(body.toString(), "val");
 }
 
+TEST_F(InjaTransformerTest, SetSpanNameNullRoute) {
+  std::string transformer_span_name = "TRANSFORMER_SPAN_NAME";
+  TransformationTemplate transformation;
+  transformation.mutable_span_transformer()->mutable_name()->set_text(transformer_span_name);
+
+  Http::TestRequestHeaderMapImpl headers{};
+  Buffer::OwnedImpl body("");
+  NiceMock<Http::MockStreamDecoderFilterCallbacks> callbacks;
+
+  InjaTransformer transformer(transformation, rng_, google::protobuf::BoolValue(), tls_);
+  std::unique_ptr<Tracing::MockSpan> mock_span = std::make_unique<Tracing::MockSpan>();
+  const std::unique_ptr<Router::MockDecorator> mock_decorator = std::make_unique<NiceMock<Router::MockDecorator>>();
+  ON_CALL(callbacks, route).WillByDefault(Return(nullptr));
+  EXPECT_CALL(callbacks, activeSpan).WillOnce(ReturnRef(*mock_span));
+  EXPECT_CALL(*mock_span, setOperation(transformer_span_name)).Times(1);
+
+  transformer.transform(headers, &headers, body, callbacks);
+}
+
+TEST_F(InjaTransformerTest, SetSpanNameNullRouteDecorator) {
+  std::string transformer_span_name = "TRANSFORMER_SPAN_NAME";
+  TransformationTemplate transformation;
+  transformation.mutable_span_transformer()->mutable_name()->set_text(transformer_span_name);
+
+  Http::TestRequestHeaderMapImpl headers{};
+  Buffer::OwnedImpl body("");
+  NiceMock<Http::MockStreamDecoderFilterCallbacks> callbacks;
+
+  InjaTransformer transformer(transformation, rng_, google::protobuf::BoolValue(), tls_);
+  std::unique_ptr<Tracing::MockSpan> mock_span = std::make_unique<Tracing::MockSpan>();
+  const std::unique_ptr<Router::MockDecorator> mock_decorator = std::make_unique<NiceMock<Router::MockDecorator>>();
+  ON_CALL(*callbacks.route_, decorator).WillByDefault(Return(nullptr));
+  EXPECT_CALL(callbacks, activeSpan).WillOnce(ReturnRef(*mock_span));
+  EXPECT_CALL(*mock_span, setOperation(transformer_span_name)).Times(1);
+
+  transformer.transform(headers, &headers, body, callbacks);
+}
+
+TEST_F(InjaTransformerTest, SetSpanNameEmptyRouteDecorator) {
+  std::string transformer_span_name = "TRANSFORMER_SPAN_NAME";
+  TransformationTemplate transformation;
+  transformation.mutable_span_transformer()->mutable_name()->set_text(transformer_span_name);
+
+  Http::TestRequestHeaderMapImpl headers{};
+  Buffer::OwnedImpl body("");
+  NiceMock<Http::MockStreamDecoderFilterCallbacks> callbacks;
+
+  InjaTransformer transformer(transformation, rng_, google::protobuf::BoolValue(), tls_);
+  std::unique_ptr<Tracing::MockSpan> mock_span = std::make_unique<Tracing::MockSpan>();
+  const std::unique_ptr<Router::MockDecorator> mock_decorator = std::make_unique<NiceMock<Router::MockDecorator>>();
+  EXPECT_CALL(*callbacks.route_, decorator).WillRepeatedly(Return(mock_decorator.get()));
+  ON_CALL(*mock_decorator, getOperation()).WillByDefault(ReturnRef(""));
+  EXPECT_CALL(callbacks, activeSpan).WillOnce(ReturnRef(*mock_span));
+  EXPECT_CALL(*mock_span, setOperation(transformer_span_name)).Times(1);
+
+  transformer.transform(headers, &headers, body, callbacks);
+}
+
+TEST_F(InjaTransformerTest, SetSpanNameNonEmptyRouteDecorator) {
+  // Ensure that if route->decorator->operation is set, that it overrides the
+  // transformer value
+  std::string transformer_span_name = "TRANSFORMER_SPAN_NAME";
+  std::string decorator_span_Name = "DECORATOR_SPAN_NAME";
+  TransformationTemplate transformation;
+  transformation.mutable_span_transformer()->mutable_name()->set_text(transformer_span_name);
+
+  Http::TestRequestHeaderMapImpl headers{};
+  Buffer::OwnedImpl body("");
+  NiceMock<Http::MockStreamDecoderFilterCallbacks> callbacks;
+
+  InjaTransformer transformer(transformation, rng_, google::protobuf::BoolValue(), tls_);
+  const std::unique_ptr<Router::MockDecorator> mock_decorator = std::make_unique<NiceMock<Router::MockDecorator>>();
+  EXPECT_CALL(*callbacks.route_, decorator).WillRepeatedly(Return(mock_decorator.get()));
+  ON_CALL(*mock_decorator, getOperation()).WillByDefault(ReturnRef(decorator_span_Name));
+  EXPECT_CALL(callbacks, activeSpan).Times(0);
+
+  transformer.transform(headers, &headers, body, callbacks);
+}
+
 const std::string NESTED_KEY =
     R"EOF(
 {
@@ -1246,7 +1326,7 @@ const std::string NESTED_KEY =
 }
 )EOF";
 
-TEST_F(InjaTransformerTest, ParseFromDynamicmeMetadata) {
+TEST_F(InjaTransformerTest, ParseFromDynamicMetadata) {
   Http::TestRequestHeaderMapImpl headers{{":method", "GET"}, {":path", "/foo"}};
   TransformationTemplate transformation;
   transformation.mutable_body()->set_text("{{dynamic_metadata(\"key:value\")}}");
@@ -1280,7 +1360,7 @@ const std::string NESTED_LIST =
 }
 )EOF";
 
-TEST_F(InjaTransformerTest, ParseFromDynamicmeMetadataList) {
+TEST_F(InjaTransformerTest, ParseFromDynamicMetadataList) {
   Http::TestRequestHeaderMapImpl headers{{":method", "GET"}, {":path", "/foo"}};
   TransformationTemplate transformation;
   transformation.mutable_body()->set_text("{{dynamic_metadata(\"key:value\")}}");
